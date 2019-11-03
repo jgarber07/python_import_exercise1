a01dd04493499cbb826663336a1ea08dd300aacc
Jessica Garber <jgarber07@gmail.com>
2019-11-03 10:49:08 -0500

    first commit

---
+++ exercise1/simple_health_income_correlation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+import pandas as pd
+import os
+os.getcwd()
+
+
+# In[45]:
+
+
+os.path.isfile('C:\\Users\Admin\Documents\KCTC\Python\healthdata2.csv')
+
+
+# In[46]:
+
+
+df1 = pd.read_csv("C:\\Users\Admin\Documents\KCTC\Python\healthdata2.csv")
+print(df1)
+
+
+# In[ ]:
+
+
+
+
